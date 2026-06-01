5f2bdd684d7b2ee92e77f0084f32f3172840a493
imrenagy-dev <imre.nagy.dev@gmail.com>
2026-06-01 02:51:57 +0200

    base yii2

---
+++ config/db-local.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=benchboss',
+    'username' => 'benchboss',
+    'password' => 'vO4i6IwurI5u',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
